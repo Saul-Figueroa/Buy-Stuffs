e765012ccc4ba2a61f66e17f3732a50e9712ffb0
Saul <sauleff@gmail.com>
2019-07-18 19:00:18 -0400

    Adding skeleton for item crud operation

--- Buy_Stuffs_SpringMVC_HibernateCRUD/src/main/java/com/revature/controller/ItemController.java
+++ Buy_Stuffs_SpringMVC_HibernateCRUD/src/main/java/com/revature/controller/ItemController.java
@@ -3,45 +3,66 @@ package com.revature.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.entity.Item;
 import com.revature.service.ItemService;
 
-@Controller
-@RequestMapping("/ItemController")
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api")
 public class ItemController {
 
 	@Autowired
 	ItemService itemService;
 	
 	//Add new item
+	@PostMapping("/items")
+	public Item addItem(@RequestBody Item item) {
+		
+		
+		return null;
+		
+	}
 	//Get all items
-	//Delete item
+	@GetMapping("/items")
+	public List<Item> getAllItems() {
+		
+		return null;
+	}
+	
 	//Get item by id
-	
-	@GetMapping("/itemdelete")
-	public String delete(Model model) {
-//		List<Item> itemList = itemService.listAllItems();
-//		System.out.println("List is :");
-//		itemList.forEach(System.out::println);
-		return "itemDelete";
+	@GetMapping("/items/{id}")
+	public Item findItem(@PathVariable int id)
+	{
+		
+		return null;
+		
+	}
+	//Delete item
+	@RequestMapping(value = "/items/{id}", method = RequestMethod.DELETE)
+	public String deleteItem(@PathVariable ("id") int id) {
+		
+		
+		return null;
+		
 	}
 	
-	@GetMapping("/itemlist")
-	public String hello(Model model) {
-		List<Item> itemList = itemService.listAllItems();
-		System.out.println("List is :");
-		itemList.forEach(System.out::println);
-		return "itemList";
+	@RequestMapping(value = "/items", method = RequestMethod.PUT)
+	public String updateItem(@RequestBody Item item) {
+		
+		return null;
+		
 	}
-
-
-	public ItemController() {
-		// TODO Auto-generated constructor stub
-	}
+	
+	
+	
 
 }
--- Buy_Stuffs_SpringMVC_HibernateCRUD/src/main/java/com/revature/dao/ItemDAOImpl.java
+++ Buy_Stuffs_SpringMVC_HibernateCRUD/src/main/java/com/revature/dao/ItemDAOImpl.java
@@ -17,93 +17,28 @@ public class ItemDAOImpl implements ItemDAO {
 	SessionFactory sessionFactory;
 
 	@Override
-	public void deleteItemById(long itemId) {
-//		Item item = session.get(Item.class, itemId);
-//		if (item != null) {
-//			session.beginTransaction();
-//			session.delete(item);
-//			session.getTransaction().commit();
-//		} else {
-//			System.out.println("Item doesn't exist with provided Id...");
-//		}
+	public void addItem(Item item) {
+		// TODO Auto-generated method stub
+		
 	}
 
+	@Override
 	public List<Item> getAllItems() {
-		ArrayList<Item> itemList = new ArrayList<>();
-
-		Query query = sessionFactory.getCurrentSession().createQuery("from Item");
-		query.setMaxResults(3);
-		itemList = (ArrayList<Item>) query.getResultList();
-		System.out.println("list in DAO " + itemList);
-		return itemList;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-//	@Override
-//	public void addItem(Item item) {
-//		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-//			session.beginTransaction();
-//			System.out.println("Item Id in Service: " + item);
-//			long id = (long)session.save(item);
-//			System.out.println("Item is created with Id: " + id);
-//			session.getTransaction().commit();
-//		} catch (HibernateException e) {
-//			e.printStackTrace();
-//		}
-//		
-//	}
-//
-//	@Override
-//	public Item fetchItemById(long itemId) {
-//		Item item = null;
-//		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-//			
-//			item = session.get(Item.class, itemId); //Returns null pointer exception if ID doesn't match
-//			
-//			System.out.println("Item Id was :" + itemId + "Object is " + item.getTitle());
-//			if (item != null) {
-//				return item;
-//			} else {
-//				System.out.println("Item doesn't exist with provided Id...");
-//			}
-//		} catch (HibernateException e) {
-//			e.printStackTrace();
-//		}
-//		return null;
-//	}
-//
-//	@Override
-//	public void deleteItemById(long itemId) {
-//		
-//		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-//			Item item = session.get(Item.class, itemId);
-//			if (item != null) {
-//				session.beginTransaction();
-//				session.delete(item);
-//				session.getTransaction().commit();
-//			} else {
-//				System.out.println("Item doesn't exist with provided Id...");
-//			}
-//		} catch (HibernateException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	@Override
-//	public void updateItemById(long itemId, Float price) { //This example is specifically for updating price
-//		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-//			Item item = session.get(Item.class, itemId);
-//			if (item != null) {
-//				item.setPrice(price);
-//				session.beginTransaction();
-//				System.out.println("Item updated: " + item);
-//				session.update(item);
-//				session.getTransaction().commit();
-//			} else {
-//				System.out.println("Item doesn't exist with provided Id...");
-//			}
-//		} catch (HibernateException e) {
-//			e.printStackTrace();
-//		}
-//		
-//	}
+	@Override
+	public Item fetchItemById(int itemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item deleteItemById(int itemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
