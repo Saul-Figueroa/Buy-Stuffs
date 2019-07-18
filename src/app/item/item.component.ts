33f6f4cb23404f4527c1b6769a0675c9c71ed3a1
Saul <sauleff@gmail.com>
2019-07-18 09:56:12 -0400

    Adding items working

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ItemServiceService} from '../item-service.service';
+import {Item} from './item';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  items:Item[]; //creating a items array
+  statusMessage: string;
+  item = new Item(); 
+
+  constructor(private _itemService:ItemServiceService) { }
 
   ngOnInit() {
+
+  }
+
+  //---------add item---------
+
+  addItem(): void{
+   this._itemService.addItem(this.item).subscribe((response) => {console.log(response);
+   }
+   );
   }
 
 }
