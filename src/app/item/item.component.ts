5f06f767c9220d48a933209e2b77786a8b1dd1db
Aditya <adityajoshiusa@gmail.com>
2019-07-18 13:06:22 -0400

    Updated routing and module

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { ItemServiceService } from '../item-service.service';
 import { Item } from './item';
-=======
-import {ItemServiceService} from '../item-service.service';
-import {Item} from './item';
->>>>>>> 3f2bb87ff4400731150c9cfa99fe422a55182a03
 
 @Component({
   selector: 'app-item',
