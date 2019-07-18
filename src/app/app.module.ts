e9bb1607666c5f12664ce7c937786fe7dff455ce
Saul <sauleff@gmail.com>
2019-07-17 21:57:57 -0400

    Adding item component, item-list component, item-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule
