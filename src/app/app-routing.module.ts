86fd05886e8fe1606dcb0d09172fc9c77427f69a
tchaoububba <tchaoububba@gmail.com>
2019-07-19 02:48:20 -0400

    Updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import {ItemComponent} from './item/item.component';
+import { ItemComponent } from './item/item.component';
 import { DeleteItemComponent } from './delete-item/delete-item.component';
 import { ItemListComponent } from './item-list/item-list.component';
 
--- src/app/delete-item/delete-item.component.ts
+++ src/app/delete-item/delete-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../item/item';
 import { ItemServiceService } from '../item-service.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-delete-item',
@@ -8,6 +10,22 @@ import { ItemServiceService } from '../item-service.service';
   styleUrls: ['./delete-item.component.css']
 })
 export class DeleteItemComponent implements OnInit {
+  constructor(private _httpService: HttpClient) {}
+
+  getItems(): Observable<any>{ //asynchronous
+    // return this._httpService.get("http://localhost:3000/items");
+    return this._httpService.get("http://localhost:8090/Rev_SpringMVC_Hello/api/items");
+  }
+
+  // 3. Get a item
+  getItemById(itemId: number): Observable<any>{
+    return this._httpService.get("http://localhost:8090/Rev_SpringMVC_Hello/api/items" + itemId)
+  }
+
+   //4. Delete a Item
+   deleteItem(itemId: number){
+    return this._httpService.delete("http://localhost:8090/Rev_SpringMVC_Hello/api/items" + itemId);
+  }
 
   // 1. Create empty items array
   public items:Item[]=[];
@@ -15,29 +33,30 @@ export class DeleteItemComponent implements OnInit {
   statusMessage:String;
   item = new Item();
 
-  // 2. Create service object
-  constructor(private _itemService:ItemServiceService) { }
+  // 2. Create service object-------------commented to allow for HttpClient
+  // constructor(private _itemService:ItemServiceService) { }
 
   // 3. return item array
-  getItems():Item[]{
-    return this.items;
-  }
+  ///////////////Commented because I took in other getItems method
+  // getItems():Item[]{
+  //   return this.items;
+  // }
 
   // 4. Call service method and assign the result in items array
   ngOnInit() {
-    this._itemService.getItems();
+    this.getItems();
   }
 
-  // Delete method
-  deleteItem(id:number){
-    this._itemService.deleteItem(id).subscribe((response) => {console.log(response);
-                                                             this.getItems();
-                                                              },
-                                                              (error) => {
-                                                                console.log(error);
-                                                                this.statusMessage = "Problem with service. Please try again later.";
-                                                              }
-                                              );
-  }
+  // Delete method-----------------------commented cuz tried again above
+  // deleteItem(id:number){
+  //   this._itemService.deleteItem(id).subscribe((response) => {console.log(response);
+  //                                                            this.getItems();
+  //                                                             },
+  //                                                             (error) => {
+  //                                                               console.log(error);
+  //                                                               this.statusMessage = "Problem with service. Please try again later.";
+  //                                                             }
+  //                                             );
+  // }
 
 }
